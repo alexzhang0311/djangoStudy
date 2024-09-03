aa01674333fe1c5bd5c948e303056f7ecb0b0100
alexzhang <317601060@qq.com>
2024-09-03 20:16:08 +0800

    DTL过滤器

--- first_project/template_filter/views.py
+++ first_project/template_filter/views.py
@@ -10,6 +10,10 @@ def index(request):
         "default":"hello",
         "default_0":[],
         "default_1":None,
-        "floatValue":3.1415926
+        "floatValue":3.1415926,
+        "script":"<script> function showAlert(message) { alert(message);}</script>",
+        "alert":"<button onclick=\"showAlert('safe过滤器测试')\">点我</button>",
+        "truncate":"创世纪滨海花园",
+        "truncate_html":"<p>模板过滤器</p>"
     }
     return render(request, "template_filter.html",context=data)
